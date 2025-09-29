963a1f8a6aa3b410fc5fb1886f7127e7c30168c6
Dimitris Zervas <dzervas@dzervas.gr>
2025-09-29 22:20:34 +0300

    Move to devenv

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  languages = {
+    rust.enable = true;
+    javascript.enable = true;
+    javascript.yarn.enable = true;
+  };
+
+  packages = with pkgs; [
+    cargo-audit
+    cargo-criterion
+    gnuplot # For benchmarks
+    hurl
+    watchexec
+    sqlx-cli
+    openssl.dev perl
+
+    libxml2
+    clang
+  ];
+
+  env = {
+    DATABASE_URL="sqlite://database.db";
+    LIBCLANG_PATH="${pkgs.libclang.lib}/lib";
+  };
+}
